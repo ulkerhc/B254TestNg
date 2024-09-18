ff6ac2a01e3b2cb38f679d65ace25831a43615ec
Ulker Huseynova <ulkerhc@gmail.com>
2024-09-18 11:34:32 +0200

    techpro3

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,11 +1,11 @@
 public class Runner {
     public static void main(String[]args){
         System.out.println("version 1");
-
         System.out.println("version2");
         System.out.println("version3");
         System.out.println("techpro1");
         System.out.println("techpro2");
+        System.out.println("techpro3");
 
     }
 }
